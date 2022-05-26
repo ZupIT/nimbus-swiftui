b61f0c25ae31d7a95f1d56faa2b5872a29eb0941
Daniel Tes <daniel@zup.com.br>
2022-05-26 14:51:58 -0300

    adds spm support (#6)

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 5.6
+
+/*
+ * Copyright 2022 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import PackageDescription
+
+let package = Package(
+  name: "NimbusSwiftUI",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "NimbusSwiftUI",
+      targets: ["NimbusSwiftUI"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+      from: "1.9.0"
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "NimbusCore",
+      path: "NimbusCore/NimbusCore.xcframework"
+    ),
+    .target(
+      name: "NimbusSwiftUI",
+      dependencies: ["NimbusCore"],
+      path: "NimbusSwiftUI/NimbusSwiftUI"
+    ),
+    .testTarget(
+      name: "NimbusSwiftUITests",
+      dependencies: [
+        "NimbusSwiftUI",
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+      ],
+      path: "NimbusSwiftUI/NimbusSwiftUITests"
+    )
+  ]
+)
